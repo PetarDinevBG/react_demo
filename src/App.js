60c5034233a02860e72ee89246d0c67236060a67
PetarDinevBG <pdinev@u.rochester.edu>
2021-08-23 18:51:44 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ColumnView extends Component{
+  render() {
+      const cardsLength = this.props.cards.length;
+      const cards = []
+      for(let i=0; i < cardsLength; i++){
+        cards.push(<Card name={this.props.cards[i]} />)
+      }
+      return (
+        <div class="column container" style={{background: "red", maxHeight: "300px", overflow: "scroll"}}>
+          <h1>{this.props.name}</h1>
+            <div class=".grid">
+                {cards}
+            </div>
+
+        </div>
+    )
+  }
+}
+
+class Card extends Component{
+    render() {
+        return(
+            <div class="item">
+                {this.props.name}
+            </div>
+        )
+    }
+}
+
+const columns  = [<ColumnView name="Col1" cards={[1,2,3,4]} />,<ColumnView name="Col2" cards={[1]}/>, <ColumnView name="Col3" cards={[1,2,3,4,5,6,7]}/>, <ColumnView name="Col4" cards={[3,4]}/>];
+
+class App extends Component {
+  render() {
+      return (
+        <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 20 }}>
+            {columns}
+        </div>
+    )
+  }
 }
 
 export default App;
